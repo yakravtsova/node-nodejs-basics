a018c2fc2be80701587ba4e2df4fbc0606fd1d19
yakravtsova <margaritaselez@yandex.ru>
2023-06-17 11:06:43 +0500

    read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,19 @@
+import fs from "fs";
+import path from "path";
+
 const read = async () => {
-    // Write your code here 
+  // Write your code here
+  const filePath = path.join("src", "fs", "files", "fileToRead.txt");
+  fs.access(filePath, fs.constants.F_OK, (err) => {
+    if (err) {
+      throw new Error("FS operation failed");
+    } else {
+      fs.readFile(filePath, "utf8", (err, data) => {
+        if (err) throw new Error("Unexpected error");
+        console.log(data);
+      });
+    }
+  });
 };
 
 await read();
