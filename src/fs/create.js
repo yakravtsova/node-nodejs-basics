1603b4f02190d49c08e930fe403c0b4b3f5e39d2
yakravtsova <margaritaselez@yandex.ru>
2023-06-17 11:04:16 +0500

    create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,19 @@
+import fs from "fs";
+import path from "path";
+
 const create = async () => {
-    // Write your code here 
+  // Write your code here
+  const filePath = path.join("src", "fs", "files", "fresh.txt");
+  fs.access(filePath, fs.constants.F_OK, (err) => {
+    if (err) {
+      fs.appendFile(filePath, "I am fresh and young", (err) => {
+        if (err) throw err;
+        console.log("Saved!");
+      });
+    } else {
+      throw new Error("FS operation failed");
+    }
+  });
 };
 
 await create();
