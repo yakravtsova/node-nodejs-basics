518490b0767ee36f7bbcda720da1f949ac9b3744
yakravtsova <margaritaselez@yandex.ru>
2023-06-17 11:08:29 +0500

    rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,25 @@
+import fs from "fs";
+import path from "path";
+
 const rename = async () => {
-    // Write your code here 
+  // Write your code here
+  const wrongFilePath = path.join("src", "fs", "files", "wrongFilename.txt");
+  const properFilePath = path.join("src", "fs", "files", "properFilename.md");
+  fs.access(wrongFilePath, fs.constants.F_OK, (err) => {
+    if (err) {
+      throw new Error("FS operation failed");
+    } else {
+      fs.access(properFilePath, fs.constants.F_OK, (err) => {
+        if (err) {
+          fs.rename(wrongFilePath, properFilePath, (err) => {
+            if (err) throw new Error("Unexpected error");
+          });
+        } else {
+          throw new Error("FS operation failed");
+        }
+      });
+    }
+  });
 };
 
 await rename();
