d10ccd38bf210f508201e7b7b5a399408cb3c1d0
yakravtsova <margaritaselez@yandex.ru>
2023-06-17 11:05:09 +0500

    delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,18 @@
+import fs from "fs";
+import path from "path";
+
 const remove = async () => {
-    // Write your code here 
+  // Write your code here
+  const filePath = path.join("src", "fs", "files", "fileToRemove.txt");
+  fs.access(filePath, fs.constants.F_OK, (err) => {
+    if (err) {
+      throw new Error("FS operation failed");
+    } else {
+      fs.unlink(filePath, (err) => {
+        if (err) throw new Error("Unexpected error");
+      });
+    }
+  });
 };
 
 await remove();
