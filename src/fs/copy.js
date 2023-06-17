cff3cbe6be7cf5023804674ab696d39b981f3334
yakravtsova <margaritaselez@yandex.ru>
2023-06-17 11:02:52 +0500

    copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,21 @@
+import fs from "fs";
+import path from "path";
+
 const copy = async () => {
-    // Write your code here 
+  // Write your code here
+  const dirPath = path.join("src", "fs", "files");
+  const dirCopyPath = path.join("src", "fs", "files_copy");
+  fs.access(dirCopyPath, fs.constants.F_OK, (err) => {
+    if (err) {
+      fs.cp(dirPath, dirCopyPath, { recursive: true }, (err) => {
+        if (err) {
+          console.error(err);
+        }
+      });
+    } else {
+      throw new Error("FS operation failed");
+    }
+  });
 };
 
 await copy();
