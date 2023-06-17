ea9704275a08e0e25ebad9243c9517a92bac1dc3
yakravtsova <margaritaselez@yandex.ru>
2023-06-17 11:05:57 +0500

    list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,21 @@
+import fs from "fs";
+import path from "path";
+
 const list = async () => {
-    // Write your code here 
+  // Write your code here
+  const filePath = path.join("src", "fs", "files");
+  fs.access(filePath, fs.constants.F_OK, (err) => {
+    if (err) {
+      throw new Error("FS operation failed");
+    } else {
+      fs.readdir(filePath, (err, items) => {
+        if (err) throw new Error("Unexpected error");
+        items.forEach((item) => {
+          console.log(item);
+        });
+      });
+    }
+  });
 };
 
 await list();
