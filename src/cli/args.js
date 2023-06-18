510a7762bce8050bf47ae36980056373dea8d05f
yakravtsova <margaritaselez@yandex.ru>
2023-06-19 01:30:09 +0500

    args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
+import { argv } from "process";
+
 const parseArgs = () => {
-    // Write your code here 
+  // Write your code here
+  let args = [];
+  for (let i = 0; i < argv.length; i++) {
+    if (argv[i].startsWith("--")) {
+      args.push(`${argv[i]} is ${argv[i + 1]}`);
+    }
+  }
+  console.log(args.join(", "));
 };
 
 parseArgs();
