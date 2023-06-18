8336bcb7e3fc50bf6981b65266b07e35b92d272d
yakravtsova <margaritaselez@yandex.ru>
2023-06-19 00:05:45 +0500

    transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,13 @@
+const reverse = (string) => {
+  return string.split("").reverse().join("");
+};
+
 const transform = async () => {
-    // Write your code here 
+  // Write your code here
+  process.stdin.on("data", (data) => {
+    data = reverse(data.toString());
+    process.stdout.write(data);
+  });
 };
 
 await transform();
