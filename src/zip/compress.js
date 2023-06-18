3d54b430abbf340f73b53c8aeeefd5a39728c737
yakravtsova <margaritaselez@yandex.ru>
2023-06-18 20:00:01 +0500

    zip/compress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,20 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+import { createGzip } from "zlib";
+
 const compress = async () => {
-    // Write your code here 
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+  const filePath = path.join(__dirname, "files", "fileToCompress.txt");
+  const archivePath = path.join(__dirname, "files", "fileToCompress.gz");
+  const handleStream = fs.createReadStream(filePath);
+  handleStream
+    .pipe(createGzip())
+    .pipe(fs.createWriteStream(archivePath))
+    .on("finish", () => {
+      console.log("Success!");
+    });
+  // Write your code here
 };
 
 await compress();
