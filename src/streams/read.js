1d6f23f7fca1616004ad5137c85cb6c9dae0e008
yakravtsova <margaritaselez@yandex.ru>
2023-06-18 23:47:08 +0500

    read stream

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,15 @@
+import { createReadStream } from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
 const read = async () => {
-    // Write your code here 
+  // Write your code here
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+  const filePath = path.join(__dirname, "files", "fileToRead.txt");
+  const readStream = createReadStream(filePath, "UTF-8");
+  readStream.on("data", (chunk) => {
+    process.stdout.write(chunk);
+  });
 };
 
 await read();
