07a499f9db62cc23d2bcb2f073d99d211733cfdb
Margarita Kravtsova (Teamlead) <margaritaselez@gmail.com>
2024-01-29 01:23:49 +0500

    feat: implement workers

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,14 +1,16 @@
 import { isMainThread } from "worker_threads";
+import { parentPort, workerData } from "worker_threads";
 
 // n should be received from main thread
-const nthFibonacci = (n) => {
-  if (isMainThread) {
-    n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
-  }
-};
+const nthFibonacci = (n) =>
+  n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
   // This function sends result of nthFibonacci computations to main thread
+  if (Math.random() > 0.5) {
+    throw new Error("Something went wrong"); //imitation of error in worker
+  }
+  parentPort.postMessage(nthFibonacci(workerData));
 };
 
 sendResult();
